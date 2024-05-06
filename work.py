c0b93a724d2ba64e4a29219da551787883b9b6cf
410411499 <410411499@gms.tku.edu.tw>
2024-05-07 06:05:25 +0800

    Update work.py

--- work.py
+++ work.py
@@ -1,9 +1,10 @@
 import numpy as np 
 
-samples = np.random.normal(-100,50,10000)
+samples = np.random.normal(-100,100,10000)
 
 mean_value =np.mean(samples)
 std_deviation =np.std(samples)
 
 print("mean",mean_value)
+print("hello world!")
 print("standard deviation",std_deviation)
